64c3b4b32962e33c23c757e47f3891b3a08606ef
Christian Kreuzberger <s4ko88@gmail.com>
2024-05-14 21:30:46 +0200

    Dynamic URL discovery for prod

--- frontend/src/TempberryAPI.js
+++ frontend/src/TempberryAPI.js
@@ -1,9 +1,22 @@
-const baseUrl = 'http://localhost:5000';
+
+const getBaseUrl = () => {
+    if (window.location.host.toString().indexOf('localhost') >= 0) {
+        return 'http://localhost:5000';
+    }
+
+    // prod
+    console.log('PROD environment');
+    return '';
+}
 
 export const fetchLiveData = () => {
-    return fetch(baseUrl + '/live_temperatures').then((response) => response.json());
+    console.log(window.location.host.toString());
+
+    return fetch(getBaseUrl() + '/live_temperatures').then((response) => response.json());
 }
 
 export const fetchRoomMapping = () => {
-    return fetch(baseUrl + '/room_sensor_id_map').then((response) => response.json());
+    console.log(window.location);
+
+    return fetch(getBaseUrl() + '/room_sensor_id_map').then((response) => response.json());
 }
